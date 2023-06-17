f3640f82158f2ce32c9229342f11c46b7f4ce2aa
William Ugalde Gamboa <wugalde@ltvco.com>
2023-06-16 23:14:07 -0600

    Práctica 7133/7134 - Parte 1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,19 @@
 class BooksController < ApplicationController
   def index
     @books = [
-      {id: 1, name: 'Libro 1'},
-      {id: 2, name: 'Libro 2'},
-      {id: 3, name: 'Libro 3'},
+      {id: 1, name: 'Libro 1', author: 'Autor 1'},
+      {id: 2, name: 'Libro 2', author: 'Autor 2'},
+      {id: 3, name: 'Libro 3', author: 'Autor 3'},
     ]
-
-    render json: @books 
   end
 
   def show
     @books = [
-      {id: 1, name: 'Libro 1'},
-      {id: 2, name: 'Libro 2'},
-      {id: 3, name: 'Libro 3'},
+      {id: 1, name: 'Libro 1', author: 'Autor 1'},
+      {id: 2, name: 'Libro 2', author: 'Autor 2'},
+      {id: 3, name: 'Libro 3', author: 'Autor 3'},
     ]
 
-    @book = @books.select { |book| book[:id].to_s == params[:id] }
-    render json: @book 
+    @book = @books.select { |book| book[:id].to_s == params[:id] }.first
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
   def index
-    render html: 'Main Page'
   end
 
   def about
-    render html: "About Page"
   end
 end
